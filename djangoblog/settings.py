86aed5432421e56fe080037b465231935623c057
Tung Tang <tangtienthanhtung@gmail.com>
2022-01-28 00:22:44 +0700

    Update setting

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -37,8 +37,8 @@ DEBUG = env.str('DJANGO_DEBUG', True)
 TESTING = len(sys.argv) > 1 and sys.argv[1] == 'test'
 
 PREPEND_WWW = True
-# ALLOWED_HOSTS = ['*', '127.0.0.1', 'tungblog-d47vt.ondigitalocean.app']
-ALLOWED_HOSTS = env("DJANGO_ALLOWED_HOSTS", default="localhost")
+ALLOWED_HOSTS = ['*', '127.0.0.1', 'tungblog-d47vt.ondigitalocean.app']
+# ALLOWED_HOSTS = env("DJANGO_ALLOWED_HOSTS", default="localhost")
 # Application definition
 
 
